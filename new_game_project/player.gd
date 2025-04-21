11391d0f7dc0f8d2f41c628b7a6dfc7ae2b9a908
JNBarnett <jnbarnett735@gmail.com>
2025-04-21 15:17:14 -0400

    Saving game data and Pool

--- new_game_project/player.gd
+++ new_game_project/player.gd
@@ -4,6 +4,11 @@ extends RigidBody2D
 @export var jump_accel:float = -500.0
 
 
+var hp: float = 10.0
+var xp: int = 1
+var mp: int = 0
+@onready var label: RichTextLabel = $"../RichTextLabel"
+
 func _ready() -> void:
 	pass
 	
@@ -16,33 +21,52 @@ func _physics_process(delta: float) -> void:
 		apply_central_impulse((Vector2(-acceleration,0.5)))
 	elif Input.is_action_just_pressed("Up"):
 		apply_central_impulse(Vector2(0, jump_accel))
+		
+		var hit = randf_range(-5.5, 5.5);
+		hp += hit;
+		var experience = randi() % 50
+		if experience > 45:
+			xp += 1;
+		var magic = randi() % 50
+		if magic > 47:
+			mp += 1
+		
+		var output = ""
+		output += "HP: " + str("%.2f" % hp) + "\n"
+		output += "XP: " + str(xp) + "\n"
+		output += "MP: " + str(mp) + "\n"
+
+		label.text = output;
 	else:
 		apply_central_impulse(Vector2.ZERO)
+	
+	if Input.is_action_just_pressed("ui_page_down"):
+		save()
+	
+	if Input.is_action_just_pressed("ui_page_up"):
+		load_data()
 
 
+func save():
+		var file:= FileAccess.open("user://game_state.json", FileAccess.WRITE)
+		var data:= {
+			"HP": hp,
+			"XP": xp,
+			"MP": mp
+		}
+		file.store_string(str(data));
+		file.close();
+		label.text = "Game Data Saved"
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-############# WS for scrolling #################
+func load_data():
+	if FileAccess.file_exists("user://game_state.json"):
+		var file:= FileAccess.open("user://game_state.json", FileAccess.READ)
+		var data = JSON.parse_string(file.get_as_text())
+		if data is Dictionary:
+			hp = data["HP"]
+			xp = data["XP"]
+			mp = data["MP"]
+			label.text = "Game Loaded"
+		else:
+			label.text = "Error Loading"
